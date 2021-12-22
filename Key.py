5fe09a174aa65df7a856ad3ee19fc547dad0f412
Inezlsya <89917337+Inezlsya@users.noreply.github.com>
2021-12-22 08:22:54 +0700

    Update Key.py

--- Key.py
+++ Key.py
@@ -2,16 +2,16 @@ from Crypto.PublicKey import RSA
 
 kunci_baru = RSA.generate(4096, e=65537)
 
-kunci_private = kunci_baru.exportKey("PEM")
+kunci_private = kunci_baru.exportKey("PEM") #untuk mengeirimkan kunci_private dalam bentuk baru disetiap py dijalankan dalam bentuk PEM
 
-kunci_publik = kunci_baru.publickey().exportKey("PEM")
+kunci_publik = kunci_baru.publickey().exportKey("PEM") #untuk mengeirimkan kunci_private dalam bentuk baru disetiap py dijalankan dalam bentuk PEM
 
-print(kunci_private)
+print(kunci_private) #untuk menghasilkan kunci_private.pem
 fd = open("kunci_private.pem", "wb")
 fd.write(kunci_private)
 fd.close()
 
-print(kunci_publik)
+print(kunci_publik) #untuk menghasilkan kunci_publik.pem
 fd = open("kunci_publik.pem", "wb")
 fd.write(kunci_publik)
 fd.close()
